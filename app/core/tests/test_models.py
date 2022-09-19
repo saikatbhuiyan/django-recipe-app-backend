b1552dfbce9384be6d3c5e372315d435dd2ebcbd
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-09-19 06:38:26 +0600

    Email ValueError and create_superuser manager

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,3 +30,18 @@ class ModelTest(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
