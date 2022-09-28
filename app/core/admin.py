56dab35f7e9ebe5591b00227545af98d0961cd32
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-09-28 12:30:25 +0600

    Create Tag model and test

--- app/core/admin.py
+++ app/core/admin.py
@@ -47,3 +47,4 @@ class UserAdmin(BaseUserAmin):
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Recipe)
+admin.site.register(models.Tag)
--- app/core/models.py
+++ app/core/models.py
@@ -54,6 +54,19 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self) -> str:
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.name
