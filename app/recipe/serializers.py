a3cb3ef39f597e32299c60dbd06f1e8f4581081f
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-10-02 00:43:43 +0600

    Ingredient List API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,12 +1,23 @@
 """
 Serializers for recipe APIs
 """
+from dataclasses import fields
 from rest_framework import serializers
 
 from core.models import (
     Recipe,
     Tag,
+    Ingredient
 )
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredients."""
+
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 class TagSerializer(serializers.ModelSerializer):
